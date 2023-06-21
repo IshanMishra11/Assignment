859709d320319c0f8578b319820f8c69f5a83445
IshanMishra11 <134917424+IshanMishra11@users.noreply.github.com>
2023-06-21 20:19:27 +0530

    assign.js

--- assign.js
+++ assign.js
@@ -1,9 +1,8 @@
-var length = 5;
-var width = 3;
-var rectangleArea = calculateArea(length, width);
-console.log(rectangleArea); // Output: 15
-
-
-
-
-  
+function calculateArea(length, width) {
+    var area = length * width;
+    return area;
+  }
+  var length = 5;
+  var width = 3;
+  var rectangleArea = calculateArea(length, width);
+  console.log(rectangleArea); // Output: 15
